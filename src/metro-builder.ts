245eb9bd1ad64c07de8ef15fef31c11a199e9614
Ashray Mehta <ashraymehta@users.noreply.github.com>
2019-04-19 14:14:18 +0530

    Ashray | Multiple lines can now have their respective stops in metro-builder.

--- src/metro-builder.ts
+++ src/metro-builder.ts
@@ -1,3 +1,4 @@
+import {groupBy} from 'lodash';
 import {Line} from './models/line';
 import {Metro} from './models/metro';
 import {LineStop} from './models/line-stop';
@@ -11,11 +12,21 @@ export class MetroBuilder {
     }
 
     public async build(): Promise<any> {
-        const stationsMap = await this.jsonFileReader.readFile(`src/assets/stations-map.json`) as object[];
-        const stops = [];
-        const line = new Line(stops);
-        const lines = <Line[]>[line];
-        stationsMap.forEach(station => stops.push(new LineStop(station['StationCode'], new Date(station['OpeningDate']))));
+        const stationsMap = await this.jsonFileReader.readFile(`src/assets/stations-map.json`) as UnparsedStationMap[];
+        const stationsGroupedByLineCodes = groupBy(stationsMap, (station: UnparsedStationMap) => station.StationCode.substr(0, 2));
+
+        const lines = Object.keys(stationsGroupedByLineCodes).map(lineCode => {
+            const stopsForLine = stationsGroupedByLineCodes[lineCode] as UnparsedStationMap[];
+            const stops = stopsForLine.map(station => new LineStop(station.StationCode, new Date(station.OpeningDate)));
+            return new Line(stops);
+        });
+
         return new Metro(lines);
     }
+}
+
+interface UnparsedStationMap {
+    StationCode: string;
+    StationName: string;
+    OpeningDate: string;
 }
--- test/metro-builder.spec.ts
+++ test/metro-builder.spec.ts
@@ -38,4 +38,32 @@ class MetroBuilderSpec {
         const expectedMetro = new Metro(expectedLines);
         expect(metro).to.deep.equal(expectedMetro);
     }
+
+    @test
+    public async shouldBuildMetroFromTheStationsJSONWhenMultipleLinesArePresent(): Promise<void> {
+        const reader = mock(JSONFileReader);
+        const metroBuilder = new MetroBuilder(instance(reader));
+        when(reader.readFile('src/assets/stations-map.json')).thenResolve([
+            {
+                StationCode: "NS1",
+                StationName: "Jurong East",
+                OpeningDate: "10 March 1990"
+            },
+            {
+                StationCode: "EW1",
+                StationName: "Pasir Ris",
+                OpeningDate: "16 December 1989"
+            }
+        ]);
+
+        const metro = await metroBuilder.build();
+
+        expect(metro).to.be.an.instanceOf(Metro);
+        const expectedLines = [
+            new Line([new LineStop('NS1', new Date('10 March 1990'))]),
+            new Line([new LineStop('EW1', new Date('16 December 1989'))])
+        ];
+        const expectedMetro = new Metro(expectedLines);
+        expect(metro).to.deep.equal(expectedMetro);
+    }
 }
