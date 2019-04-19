741eee436d44ba65b3a41471aca7216f59f4948f
Ashray Mehta <ashraymehta@users.noreply.github.com>
2019-04-19 13:07:59 +0530

    Ashray | Added json-file-reader to read files with JSON content.

---
+++ src/utils/json-file-reader.ts
@@ -0,0 +1,9 @@
+import {readFile} from 'fs';
+import {promisify} from 'util';
+const readFileAsync = promisify<string, string, string>(readFile);
+
+export class JSONFileReader {
+    public async readFile(filePath: string): Promise<object> {
+        return JSON.parse(await readFileAsync(filePath, 'utf-8'));
+    }
+}
---
+++ test/utils/json-file-reader.spec.ts
@@ -0,0 +1,27 @@
+import {expect} from 'chai';
+import {suite, test} from 'mocha-typescript';
+import {JSONFileReader} from '../../src/utils/json-file-reader';
+
+@suite
+class JsonFileReaderSpec {
+    @test
+    public async shouldReadJSONFile(): Promise<void> {
+        const reader = new JSONFileReader();
+
+        const result = await reader.readFile('test/assets/test-stations.json');
+
+        expect(result).to.be.an.instanceOf(Object);
+        expect(result).to.deep.equal([
+            {
+                "StationCode": "NS1",
+                "StationName": "Jurong East",
+                "OpeningDate": "10 March 1990"
+            },
+            {
+                "StationCode": "NS2",
+                "StationName": "Bukit Batok",
+                "OpeningDate": "10 March 1990"
+            }
+        ]);
+    }
+}
