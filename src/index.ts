ed481d5ca23f8510a42dcc3489f652191a95935b
Ashray Mehta <ashraymehta@users.noreply.github.com>
2019-04-21 16:21:07 +0530

    Ashray | Added commander.js to get inputs from command line.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log(`Hello World!`);
+import commander = require('commander');
+
+commander.command('route <source> <destination>', null, {isDefault: true})
+    .description('Find one or more routes between stations of the Singapore MRT')
+    .action((source, destination) => {
+        console.log(`Source: ${source}`);
+        console.log(`Destination: ${destination}`);
+    });
+commander.parse(process.argv);
